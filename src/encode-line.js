31499f5a43024ed2d548cdc84c8914fb0900c15b
Maracash41 <9531945@gmail.com>
2023-12-23 16:10:31 +0300

    feat: Encode Line - Done!

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,11 +10,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
 
+function encodeLine(str) {
+  if (!str) return '';
+  let count = 0;
+  let result = '';
+	for (let i = 0; i < str.length; i++) {
+  const currentChar = str[i];
+  const futureChar = str[i+1];
+	count += 1;
+  
+  if(currentChar !== futureChar) {
+    if (count === 1) {
+      result += currentChar;
+      count = 0;
+    } else {
+        result += count + currentChar;
+    	  count = 0;
+    }
+  } 
+} 
+  return result;
+}
 module.exports = {
   encodeLine
 };
